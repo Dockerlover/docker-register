7ef14be429bc7f46a2e2d787aa2af14b916cd03e
刘红 <liuhong1.happy@163.com>
2015-06-21 20:53:51 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,4 @@
 HOST_IP=$(hostname --all-ip-addresses | awk '{print $1}')
 ETCD_HOST = $HOST_IP:4001
 
-docker run -it  --name register --rm  docker-register \
--e ETCD_HOST=$ETCD_HOST -e HOST_IP=$HOST_IP  \
--v /var/run:/var/run /bin/bash
+docker run -it  --name register --rm   -e ETCD_HOST=$ETCD_HOST -e HOST_IP=$HOST_IP  -v /var/run:/var/run docker-register /bin/bash
