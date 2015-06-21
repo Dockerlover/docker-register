782b7b81fb202379053592f5283e3087d041b7bb
刘红 <liuhong1.happy@163.com>
2015-06-21 17:15:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,43 @@
+from docker import Client
+import etcd
+import time
 
+POLL_TIMEOUT=5
+
+def get_imagess:
+  c = Client(base_url='unix://var/run/docker.sock')
+  images = c.images()
+  return images
+  
+def get_containers:
+  c = Client(base_url='unix://var/run/docker.sock')
+  containers = c.containers(all=true)
+  return containers
+
+def refresh_containers(containers):
+  print containers
+  return containers
+  
+def container_changed:
+  # 发送最新更新时间，以便做好记录
+  
+
+if __name__ == "__main__":
+    current_containers = {}
+    while True:
+        try:
+            containers = get_containers()
+
+            if not containers or containers == current_containers:
+                refresh_containers(containers)
+                time.sleep(POLL_TIMEOUT)
+                continue
+
+            print "containers changed. reload haproxy"
+            container_changed()
+            current_services = services
+
+        except Exception, e:
+            print "Error:", e
+
+        time.sleep(POLL_TIMEOUT)
