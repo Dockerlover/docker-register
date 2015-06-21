7d0e213cb5cd08d48818a8d09c76ded7140c25eb
刘红 <liuhong1.happy@163.com>
2015-06-21 21:14:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -60,10 +60,10 @@ def refresh_containers(containers):
     
     if(port_public_port!=None and container_name!=None):
       _prefix = '/services/'+container_name
-      client.write(_prefix, None, dir = True)
+      client.write(_prefix, dir=True)
       client.write(_prefix+'/image', container_image)
       client.write(_prefix+'/status', container_status)
-      client.write(_prefix+'/ports', None, dir = True)
+      client.write(_prefix+'/ports', dir=True)
       for port in service_ports:
         _prefix = _prefix+"/ports"
         client.write(_prefix+'/port', port.get("public_port"))
@@ -75,7 +75,7 @@ def refresh_containers(containers):
 if __name__ == "__main__":
     host, port = get_etcd_addr()
     client = etcd.Client(host=host, port=int(port))
-    services = client.write('/services',None, dir = True)
+    services = client.write('/services', dir=True)
     while True:
         try:
             containers = get_containers()
