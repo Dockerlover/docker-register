85cf8e8f1ce6afae72ee5f1d6fc30247ab6e9f6a
刘红 <liuhong1.happy@163.com>
2015-06-22 10:04:10 +0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,6 @@
+HOST_IP=$(hostname --all-ip-addresses | awk '{print $1}')
+ETCD_HOST=$HOST_IP:4001
+
+docker run -it  -d --name register \
+-e ETCD_HOST=$ETCD_HOST -e HOST_IP=$HOST_IP  \
+-v /var/run:/var/run docker-register
