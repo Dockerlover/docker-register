628eb97729d8bc1266bc4c9b8fb6ee1b39d2181c
刘红 <liuhong1.happy@163.com>
2015-06-22 12:09:20 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@ ETCD_HOST=$HOST_IP:4001
 
 docker run -it  -d --name register \
 -e ETCD_HOST=$ETCD_HOST -e HOST_IP=$HOST_IP  \
+-v /var/docker_images/docker-register:/code \
 -v /var/run:/var/run docker-register
